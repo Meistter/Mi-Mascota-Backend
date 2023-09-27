bad68f490bff028244c90c99a6517b44bb99918a
meistter <josueperez723@gmail.com>
2023-09-27 19:47:37 -0400

    api version control

--- routes/products.router.js
+++ routes/products.router.js
@@ -17,4 +17,18 @@ router.get('/',(req,res)=>{
   }
   res.json(products)
 })
+
+router.post('/',(req,res)=>{
+  const products= []
+  const { size } = req.query
+  const limit = size || 10
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(),10),
+      image: faker.image.url()
+    })
+  }
+  res.json(products)
+})
 module.exports = router
