742f2039c5e7b72d772681933c1d1528b1de9271
meistter <josueperez723@gmail.com>
2023-09-27 20:04:59 -0400

    patch method

--- routes/products.router.js
+++ routes/products.router.js
@@ -25,4 +25,24 @@ router.post('/',(req,res)=>{
     data: body
   })
 })
+
+router.patch('/:id',(req,res)=>{
+  const { id } = req.params
+  const body= req.body  
+  res.json({
+    message: 'update',
+    data: body,
+    id
+  })
+})
+
+router.delete('/:id',(req,res)=>{
+  const { id } = req.params
+  const body= req.body  
+  res.json({
+    message: 'delete',
+    id
+  })
+})
+
 module.exports = router
