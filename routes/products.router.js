82198290aca9a1c73fc863910569ed0a81d8cd5a
meistter <josueperez723@gmail.com>
2023-10-09 19:37:02 -0400

    making services

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,48 +1,44 @@
 const express = require("express");
-const { faker } = require('@faker-js/faker')
+const ProductService = require('../services/product.service')
 
 const router = express.Router()
+const service = new ProductService()
+// para poder usar el servicio debemos crear una instancia de el
 
-
-router.get('/',(req,res)=>{
-  const products= []
-  const { size } = req.query
-  const limit = size || 10
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(),10),
-      image: faker.image.url()
-    })
-  }
+router.get('/', (req, res) => {
+  const products = service.find()
   res.json(products)
 })
 
-router.post('/',(req,res)=>{
-  const body= req.body  
-  res.json({
-    message: 'created',
-    data: body
-  })
+router.post('/', (req, res) => {
+  const body = req.body
+  const newProduct = service.create(body)
+  res.status(201).json(newProduct)
 })
 
-router.patch('/:id',(req,res)=>{
+router.patch('/:id', (req, res) => {
   const { id } = req.params
-  const body= req.body  
-  res.json({
-    message: 'update',
-    data: body,
-    id
-  })
+  const body = req.body
+  const product = service.update(id, body)
+  res.json(product)
+})
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+  const product = service.findOne(id)
+  if(product)
+  {
+    res.json(product)
+  }else{
+    res.status(404).json({
+    message: 'No encontrado'
+    })
+  }
 })
 
-router.delete('/:id',(req,res)=>{
+router.delete('/:id', (req, res) => {
   const { id } = req.params
-  const body= req.body  
-  res.json({
-    message: 'Eliminar',
-    id
-  })
+  const product = service.delete(id)
+  res.json(product)
 })
 
 module.exports = router
